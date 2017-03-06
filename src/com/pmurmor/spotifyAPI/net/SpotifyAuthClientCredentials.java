966c5f3e9fd9b9c8790ac1aff0625d6fcd5c5968
pmurcia <p.murcia.morilla@gmail.com>
2017-03-06 19:54:54 +0100

    Added connection to Spotify Web API, and works!

--- src/com/pmurmor/spotifyAPI/net/SpotifyAuthClientCredentials.java
+++ src/com/pmurmor/spotifyAPI/net/SpotifyAuthClientCredentials.java
@@ -2,33 +2,61 @@ package com.pmurmor.spotifyAPI.net;
 
 import java.net.*;
 import java.util.*;
+import java.io.*;
 
 public class SpotifyAuthClientCredentials {
+
+	private String clientCredentials;
+	private HttpURLConnection connection;
 	
 	public static void main(String args[])
 	{
-		try {
-			URL authorizationURL = new URL("https://accounts.spotify.com/api/token");
-			HttpURLConnection connection = (HttpURLConnection) authorizationURL.openConnection();
-			
-			String userCredentials = "client_id:consumer_id";
-			String basicAuth = "Basic " + Base64.getEncoder().encodeToString(userCredentials.getBytes());
-
-			System.out.println(Base64.getEncoder().encodeToString(userCredentials.getBytes()));
-			System.out.println(basicAuth);
-						
-			connection.setRequestProperty("Authorization", basicAuth);
-			connection.setRequestMethod("GET");
-			
-			connection.connect();
-			
-			System.out.println(connection.getRequestProperties());
-			
-			System.out.println(connection.getHeaderField(0));
-			System.out.println(connection.getRequestMethod());
-			System.out.println(connection.getHeaderFields());			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}		
+		try
+		{
+			new SpotifyAuthClientCredentials("","");
+		} 
+		catch (Exception e)
+		{
+			System.out.println("Error");
+		}
+	}
+	
+	public SpotifyAuthClientCredentials(String clientId, String clientSecret) throws MalformedURLException, IOException 
+    {
+		this.setCredentials(clientId, clientSecret);
+        this.setUpUrl();
+        System.out.println(this.getResponseText());
+    }
+	
+	public void setUpUrl() throws MalformedURLException, IOException
+	{
+		URL u = new URL("https://accounts.spotify.com/api/token?grant_type=client_credentials");
+        connection = (HttpURLConnection) u.openConnection();
+        
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Authorization", clientCredentials);
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+	}
+	
+	public void setCredentials(String clientId, String clientSecret)
+	{
+		String credentials = clientId + ":" + clientSecret;
+		this.clientCredentials = "Basic " + Base64.getEncoder().encodeToString(credentials.getBytes());
+	}
+	
+	public String getResponseText() throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        
+        while(s!=null)
+        {
+            sb.append(s + "\n");
+            s = br.readLine();
+        }
+        
+        return sb.toString();
 	}
 }
